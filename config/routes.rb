ffb7fb80579b998172e2a374c143a93117634d52
Giovanni Rodriguez <milton.rodriguezg@campusucc.edu.co>
2018-11-24 21:26:10 +0000

    salvar codigo

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :posts do  
+  resources :comments
+end
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  root 'posts#index'
+  get 'about' => 'pages#about'
+  
+end
